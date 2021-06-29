c8890ae1d8cfb39ab06e806d2d78bfd2650dba3f
AO Industries, Inc <support@aoindustries.com>
2021-06-29 03:09:41 -0500

    Now supports Java 9+ modules.

---
+++ src/main/java/module-info.java
@@ -0,0 +1,28 @@
+/*
+ * pragmatickm-procedure-all - Convenience POM to include most PragmaticKM Procedure features, not including documentation or examples.
+ * Copyright (C) 2021  AO Industries, Inc.
+ *     support@aoindustries.com
+ *     7262 Bull Pen Cir
+ *     Mobile, AL 36695
+ *
+ * This file is part of pragmatickm-procedure-all.
+ *
+ * pragmatickm-procedure-all is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * pragmatickm-procedure-all is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with pragmatickm-procedure-all.  If not, see <http://www.gnu.org/licenses/>.
+ */
+module com.pragmatickm.procedure.all {
+	// Direct
+	requires transitive com.pragmatickm.procedure.model; // <groupId>com.pragmatickm</groupId><artifactId>pragmatickm-procedure-model</artifactId>
+	requires transitive com.pragmatickm.procedure.servlet; // <groupId>com.pragmatickm</groupId><artifactId>pragmatickm-procedure-servlet</artifactId>
+	requires transitive com.pragmatickm.procedure.taglib; // <groupId>com.pragmatickm</groupId><artifactId>pragmatickm-procedure-taglib</artifactId>
+}
